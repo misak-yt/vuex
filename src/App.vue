f3af4fc2863d072731bd9fd766afd306cc91c84e
misak <runnrunn400.rina@gmail.com>
2022-08-12 22:21:23 +0900

    vuexでカウントしてみた

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <input type="button" value="-" @click="minus" />
+    {{ count }}
+    <input type="button" value="+" @click="plus" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  computed: {
+    count() {
+      return this.$store.state.count
+    }
+  },
+  methods: {
+    minus() {
+      this.$store.commit('minus')
+    },
+    plus() {
+      this.$store.commit('plus')
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
